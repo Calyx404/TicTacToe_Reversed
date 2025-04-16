0406d243867af91fea2a7bb8450cc3e9e3ac5858
Calyx404 <127593133+Calyx404@users.noreply.github.com>
2025-04-16 23:17:15 +0800

    fix : organize code syntax and logic

--- game/menu.py
+++ game/menu.py
@@ -1,10 +1,8 @@
-from colorama import init, Fore, Style, Back
-
 main_menu = {
     1: "Play vs. Bot",
     2: "Play vs. Friend",
     3: "Achievements",
-    4: "Mechanics",
+    4: "How to Play",
     5: "Settings",
     6: "Quit"
 }
@@ -42,27 +40,35 @@ class Menu:
 
     def display(self) -> None:
 
-        init(autoreset=True)
+        from colorama import Fore, Style
 
-        break_line = lambda border, char : f'{border}{char * ((self.width - (len(border) * 2)) + 1)}{border}'
+        def break_line(border: str, char: str) -> str:
+            return f'{border}{char * ((self.width - (len(border) * 2)) + 1)}{border}'
 
-        print(Style.BRIGHT + "\n" + break_line(border = "  ", char = "-"))
-        print(Style.BRIGHT + '|' + Fore.BLUE + f'{self.title.upper():^{self.width - 1}}' + Style.RESET_ALL + Style.BRIGHT + '|')
-        print(Style.BRIGHT + break_line(border = "  ", char = "-"))
+        # Heading
+        print(f"\n{Style.BRIGHT}{break_line(border = "  ", char = "-")}")
+        print(f"{Style.BRIGHT}|{Fore.BLUE}{f"{self.title.upper():^{self.width - 1}}"}{Fore.RESET}|")
 
-        print(Style.BRIGHT + break_line(border = "|", char = " "))
+        print(f"{Style.BRIGHT}{break_line(border = "  ", char = "-")}")
+        print(f"{Style.BRIGHT}{break_line(border = "|", char = " ")}")
 
-        for row in range((len(self.selection) + self.columns - 1) // self.columns):
-            print(Style.BRIGHT + "|", end="")
+        # Options
+        total_options = len(self.selection)
+        rows = (total_options + self.columns - 1) // self.columns
+        col_width = (self.width - self.columns) // self.columns
+
+        for row in range(rows):
+            print(f"{Style.BRIGHT}|", end="")
 
             for col in range(1, self.columns + 1):
                 option = col + (self.columns * row)
 
                 if option in self.selection:
-                    print(Style.BRIGHT + Fore.BLUE + f'{f'[{option:1}] {self.selection[option]}':^{(self.width - (self.columns)) // self.columns}}' + Style.RESET_ALL + Style.BRIGHT + '|', end="")
+                    text = f'[{option:1}] {self.selection[option]}'
+                    print(f"{Style.BRIGHT}{Fore.BLUE}{f'{text:^{col_width}}'}{Fore.RESET}|", end="")
                 else:
-                    print(Style.BRIGHT + f'{" ":^{(self.width - (self.columns)) // self.columns}}|', end="")
+                    print(f"{Style.BRIGHT}{" ":^{col_width}}|", end="")
 
-            print(Style.BRIGHT + '\n' + break_line(border = "|", char = " "))
+            print(f"\n{Style.BRIGHT}{break_line(border = "|", char = " ")}")
 
-        print(Style.BRIGHT + break_line(border = "  ", char = "-") + "\n")
+        print(f"{Style.BRIGHT}{break_line(border = "  ", char = "-")}\n")
