698129c3a16cdf93ba18ddb2a117c5b6d1fcef93
Calyx404 <127593133+Calyx404@users.noreply.github.com>
2025-04-16 23:13:48 +0800

    feat : create system log handler

--- utils/log_handler.py
+++ utils/log_handler.py
@@ -1,7 +1,8 @@
-from time import ctime
+from datetime import datetime
 
-log_filepath = r'..\logs\game.log'
+log_filepath = r'.\logs\game.log'
 
-def log(activity:str) -> None:
-    with open(log_filepath, mode = "a", newline = "") as logbook:
-        log = logbook.write(f'[{ctime()}] Log : {activity}')
+def log(level:str, activity:str) -> None:
+
+    with open(log_filepath, mode = "a") as logbook:
+        log = logbook.write(f'[{datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')}] [{level.upper()}] : {activity}\n')
